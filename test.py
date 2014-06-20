73e68fecc2180db2dd7937628f7dd0d559eada73
Tyler Schicke <tschicke@optonline.net>
2014-06-20 18:23:34 -0400

    worked on finding data

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
-clear = lambda: os.system('cls')
+clear = lambda: os.system('clear')
 
 
 import bpy
@@ -10,22 +10,73 @@ def printFunc():
     clear()
     mesh.calc_tessface()
     
+    vertices = []
+    UVs = []
+    normals = []
+    
+    outVertices = []
+    outUVs = []
+    outNormals = []
+    outIndices = []
+    
     tris = 0
     
-    indexList = []
+    triangulatedIndices = []
     
+    #Triangulate faces
     for face in mesh.tessfaces:
         if(len(face.vertices) == 3):
-            indexList.append(face.vertices[:])
+            triangulatedIndices.append(face.vertices[:])
+            uvArray = mesh.tessface_uv_textures.active.data[face.index]
+            UVs.append(uvArray.uv[0][:])
+            UVs.append(uvArray.uv[1][:])
+            UVs.append(uvArray.uv[2][:])
         else:
-            indexList.append((face.vertices[0], face.vertices[1], face.vertices[2]))
-            indexList.append((face.vertices[2], face.vertices[3], face.vertices[0]))
+            triangulatedIndices.append((face.vertices[0], face.vertices[1], face.vertices[2]))
+            triangulatedIndices.append((face.vertices[2], face.vertices[3], face.vertices[0]))
+            uvArray = mesh.tessface_uv_textures.active.data[face.index]
+            UVs.append(uvArray.uv[0][:])
+            UVs.append(uvArray.uv[1][:])
+            UVs.append(uvArray.uv[2][:])
+            UVs.append(uvArray.uv[3][:])
     
-    for tri in indexList:
+    #Print face vertices (temp)
+    for tri in triangulatedIndices:
         tris += 1
         print(tri)
             
     print("Number of tris: %i" % tris)
     
+    #Populate vertices and normals arrays
+    for v in mesh.vertices:
+        vertices.append(v.co)
+        normals.append(v.normal)
+    #Populate UVs
+    #for uv in mesh.uv_layers.active.data:
+    #    UVs.append(uv.uv)
+    
+    print("%i %i %i" % (len(vertices), len(UVs), len(normals)))
+    
+    for v in vertices:
+        print("v %.6f %.6f %.6f" % (v[0], v[1], v[2]))
+    
+    for uv in UVs:
+        print("t %.6f %.6f" % (uv[0], uv[1]))
+    
+    for n in normals:
+        print("n %.6f %.6f %.6f" % (n[0], n[1], n[2]))
     
 printFunc()
+'''
+clear()
+mesh.calc_tessface()
+#print(dir(mesh.tessface_uv_textures.active.data[0].uv[0][0]))
+print(len(mesh.tessface_uv_textures.active.data))
+print(len(mesh.uv_layers.active.data))
+#print(mesh.tessface_uv_textures.active.data[0].uv[7][0])
+
+for face in mesh.tessface_uv_textures.active.data:
+    for vertex in face.uv:
+        print(vertex[:])
+    print()
+'''
